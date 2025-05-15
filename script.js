d5c049935cf959f4ddbf895a1e618353c28df6cd
FrancisMurayaDev <francismurayadev@gmail.com>
2025-05-15 11:53:56 +0300

    use for each

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
@@ -15,9 +14,11 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener('click', openModal);
-}
+// for (let i = 0; i < btnOpenModal.length; i++) {
+//   btnOpenModal[i].addEventListener('click', openModal);
+// }
+
+btnOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
