f79608bbab0214ce7f2790046414dc66d840bdd8
FrancisMurayaDev <francismurayadev@gmail.com>
2025-05-15 09:59:28 +0300

    refactor code

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.shadow-modal');
 
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
 for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnOpenModal[i].addEventListener('click', openModal);
 }
 
-btnCloseModal.addEventListener('click', function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
 
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
